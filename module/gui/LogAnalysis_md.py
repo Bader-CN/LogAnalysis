606c2b2de5ad8a285604fd3a0e86432687c39071
Bader <zhanghong.personal@outlook.com>
2022-08-01 20:50:53 +0800

    Update LogAnalysis_md.py
    change start/end time function

--- module/gui/LogAnalysis_md.py
+++ module/gui/LogAnalysis_md.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from PySide6.QtCore import QDateTime
 from PySide6.QtWidgets import QMainWindow
 from module.gui.LogAnalysis_ui import Ui_MainWindow
 
@@ -15,3 +16,15 @@ class LogAnalysisMain(QMainWindow):
         self.ui = Ui_MainWindow()
         # 初始化界面
         self.ui.setupUi(self)
+
+        # 设置时间
+        self.set_start_end_time()
+
+    # 调整软件界面
+    def set_start_end_time(self):
+        """
+        软件启动时, 设置 Start/End Time 的显示时间
+        :return:
+        """
+        self.ui.date_start_time.setDateTime(QDateTime.addDays(QDateTime.currentDateTime(), -30))
+        self.ui.date_end_time.setDateTime(QDateTime.currentDateTime())
