b80b8c9426c47b0ba0c7577205435240d0ab690e
Bader <zhanghong.personal@outlook.com>
2022-08-17 17:50:07 +0800

    Create allCategory.py

---
+++ rules/allCategory.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+category_dict = {
+    "MicroFocus": {
+        "IMG": ["Connected Backup(CBK)", "IDOL"],
+        "ITOM": ["Operations Agent(OA)", "Operations Bridge Manager(OBM)"], },
+
+    "Company1": {"ProductLine1": ["ProductName1", "ProductName2"],}
+}
