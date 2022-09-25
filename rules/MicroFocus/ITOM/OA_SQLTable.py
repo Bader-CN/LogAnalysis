2174e4e6491319e516c2bfea3abdfaab331ed4af
Bader <zhanghong.personal@outlook.com>
2022-09-25 22:35:24 +0800

    Update OA_SQLTable.py

--- rules/MicroFocus/ITOM/OA_SQLTable.py
+++ rules/MicroFocus/ITOM/OA_SQLTable.py
@@ -15,5 +15,27 @@ class FileHash(BASE):
     filepath = Column(String, unique=True)
     hash = Column(String, unique=True)
 
+    # 外联关系, 此关系仅在 Python 中存在
+    OASystem = relationship("System")
+
     def __repr__(self):
         return "<FileHash(id='%s', filepath='%s', hash='%s')>" % (self.id, self.filepath, self.hash)
+
+class System(BASE):
+    """
+    OA System.txt
+    """
+    __tablename__ = "system"
+
+    # 表定义
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    file_id = Column(Integer, ForeignKey("filehash.id"))
+    log_line = Column(Integer)
+    log_time = Column(DateTime)
+    log_level = Column(String)
+    log_comp = Column(String)
+    log_cont = Column(String)
+
+    def __repr__(self):
+        return "<System(file_id='%s', log_line='%s', log_time='%s', log_level='%s', log_comp='%s', log_cont='%s')>" % (self.file_id, self.log_line, self.log_time, self.log_level, self.log_comp, self.log_cont)
+    
